22a9eb103f7dd5addce68a49f0358d4fd6465382
only1thor <>
2024-10-26 14:45:25 +0200

    WIP build from source. (to get driver to run)

--- pkgs/by-name/xr/xrlinuxdriver/package.nix
+++ pkgs/by-name/xr/xrlinuxdriver/package.nix
@@ -1,41 +1,31 @@
-{ lib, stdenv, fetchurl, makeWrapper, autoPatchelfHook, libusb1, curl, openssl, libevdev, json_c }:
+{ lib, stdenv, fetchFromGitHub, makeWrapper, autoPatchelfHook, libusb1, curl, openssl, libevdev, json_c, cmake, pkg-config, python3 }:
 
-stdenv.mkDerivation rec {
+let
+  pythonEnv = python3.withPackages (ps: [ ps.pyyaml ]);
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "xrlinuxdriver";
   version = "0.12.0.1";
 
-  src = fetchurl {
-    url = "https://github.com/wheaney/XRLinuxDriver/releases/download/v${version}/xrDriver-x86_64.tar.gz";
-    hash = "sha256-drgfbEknX076v1bEIY+DsGWrCnJaHIXVzKZAxB0RyAQ=";
+  src = fetchFromGitHub {
+    repo = "XRLinuxDriver";
+    owner = "wheaney";
+    rev = "v${finalAttrs.version}";
+    fetchSubmodules = true;
+    hash = "sha256-g8cYjnkLX0ArbU8pV+EbzZBMqovUzRPuEpg+Wjf3LZE=";
   };
 
-  nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
-  buildInputs = [ libusb1 stdenv.cc.libc stdenv.cc.cc curl openssl libevdev json_c ];
+  nativeBuildInputs = [ cmake pkg-config pythonEnv ];
+  buildInputs = [ libusb1 curl openssl libevdev json_c ];
 
   #dontUnpack = true;
   #dontConfigure = true;
   #dontBuild = true;
 
-  # this is mostly downloading a pre-built artifact
-  preferLocal = true;
-
-  postPatch = ''
-    substituteInPlace systemd/xr-driver.service --replace-fail "Environment=LD_LIBRARY_PATH={ld_library_path}" "" --replace-fail "ExecStart={bin_dir}/xrDriver" "ExecStart=$out/bin/xrDriver"
-  '';
-
   installPhase = ''
-    runHook preInstall
-    echo "installing"
-    mkdir -p $out/share
-    cp -r udev $out/share
-    cp -r bin $out
-    cp -r lib $out
-    mkdir -p $out/lib/systemd
-    cp -r systemd $out/lib/systemd/system
-    rm -rf $out/bin/{xr_driver_uninstall,user}
-    runHook postInstall
-  '';
-
+    mkdir -p $out/bin
+    cp xrDriver $out/bin
+    '';
 
   doInstallCheck = false;
   # The default release is a script which will do an impure download
@@ -43,6 +33,16 @@ stdenv.mkDerivation rec {
   installCheckPhase = ''
     #$out/bin/mill --help > /dev/null
     echo "install check phase"
+  '';
+
+  postInstall = ''
+    echo "post install, out dir is:"
+    echo $out
+    echo "pwd is:"
+    pwd
+    echo "ls is:"
+    find
+    false
   '';
 
   meta = with lib; {
@@ -56,4 +56,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [  ];
     platforms = lib.platforms.all;
   };
-}
+})
