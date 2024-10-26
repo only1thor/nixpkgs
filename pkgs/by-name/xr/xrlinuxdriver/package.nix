dbe7cf45346e518a0aad65261cc68f8b14b753ee
only1thor <>
2024-10-26 17:15:36 +0200

    it now breaks in fixupPhase, with interesting error error: RPATH of binary /nix/store/qsdphvbdwy1mqmasw71q3dwmy57r7z4w-xrlinuxdriver-0.12.0.1/bin/xrDriver contains a forbidden reference to /build/

--- pkgs/by-name/xr/xrlinuxdriver/package.nix
+++ pkgs/by-name/xr/xrlinuxdriver/package.nix
@@ -23,25 +23,38 @@ stdenv.mkDerivation (finalAttrs: {
   #dontBuild = true;
 
   installPhase = ''
+    echo "### install phase ###"
     mkdir -p $out/bin
     cp xrDriver $out/bin
+    cp ../bin/xr_driver_cli $out/bin
+    cp ../bin/xr_driver_verify $out/bin
+    mkdir -p $out/share
+    cp -r ../udev $out/
+    cp -r ../lib $out/
+    mkdir -p $out/lib/systemd/system
+    cp -r ../systemd $out/lib/systemd/system
+    echo "### install phase done ###"
     '';
 
   doInstallCheck = false;
   # The default release is a script which will do an impure download
   # just ensure that the application can run without network
   installCheckPhase = ''
+    echo " ### install check phase ###"
     #$out/bin/mill --help > /dev/null
     echo "install check phase"
+    echo " ### install check phase done ###"
   '';
 
   postInstall = ''
+    echo "### post install ###"
     echo "post install, out dir is:"
     echo $out
     echo "pwd is:"
     pwd
     echo "ls is:"
     find
+    echo "### post install done ###"
     false
   '';
 
